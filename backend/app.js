5b1274378cbf8473e265d5964f286ff0704091b2
EC2 Default User <ec2-user@ip-172-31-20-198.us-west-1.compute.internal>
2020-10-10 21:28:06 +0000

    Config changes for deployment

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,8 @@ var cors = require('cors');
 app.set('view engine', 'ejs');
 
 //use cors to allow cross origin resource sharing
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+//app.use(cors({ origin: 'http://localhost:3001', credentials: true }));
+app.use(cors({ origin: 'http://13.56.140.221:3000', credentials: true }));
 
 //use express session to maintain session data
 app.use(session({
@@ -22,16 +23,17 @@ app.use(session({
 // app.use(bodyParser.urlencoded({
 //     extended: true
 //   }));
-app.use(bodyParser.json());
 
 //Allow Access Control
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://13.56.140.221:3000');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
     res.setHeader('Cache-Control', 'no-cache');
     next();
   });
+
+app.use(bodyParser.json());
 
   module.exports = app;
